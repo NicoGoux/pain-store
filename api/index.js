9c32b68fd75f154b5ac2180aa61cd71dd1ec7f53
Nicolas Goux <nicolasgoux2000@gmail.com>
2023-05-17 12:22:53 -0300

    Configuracion de CORS

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 //Config
 import { connectDB } from './config/database.js';
@@ -19,6 +20,26 @@ dotenv.config();
 const port = process.env.PORT || '3030';
 
 connectDB();
+
+//cors
+const whitelist = [
+	'http://localhost:3000',
+	'https://painstore.netlify.app',
+	'http://190.246.87.32',
+];
+const cordsOptions = {
+	origin: function (origin, callback) {
+		cLog(`[connection] from: ${origin}`);
+		if (whitelist.includes(origin) || !origin) {
+			callback(null, true);
+		} else {
+			callback(new Error('CORS Error'));
+		}
+	},
+};
+
+app.use(cors(cordsOptions));
+//Routing
 
 app.use(express.json());
 app.use(passport.initialize());
